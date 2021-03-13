5f45a4d32ff9e547afaf6d9000242423550f6d3c
chaitanyamean <chaitanyamean@gmail.com>
2021-03-14 02:45:03 +0530

    added validate sub sequence ---  Array

---
+++ Array/validateSubSequence.js
@@ -0,0 +1,17 @@
+
+
+
+/// Validate Subsequence array
+
+
+function validateSubsequence(arr, seq) {
+    let seqId = 0
+    for(let value of arr) {
+        if(seqId == seq.length) break;
+        if(seq[seqId] == value) seqId++ 
+    }
+    return seqId == seq.length
+}
+
+
+validateSubsequence([5,1,22,25,6,-1,8,10], [1,6,8,10])
