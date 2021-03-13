04c6913cb8c53cffb713a82d693a0c4d38178150
chaitanyamean <chaitanyamean@gmail.com>
2021-03-14 01:39:53 +0530

    added two sum array problem

---
+++ Array/twoSumArray.js
@@ -0,0 +1,21 @@
+function twoSumArray(array, targetSum) {
+    array.sort((a,b) => a -b)
+    let left = 0;
+    let right = array.length - 1
+    while(left < right) {
+        let currentSum = array[left] + array[right]
+        if (currentSum == targetSum) {
+            return [array[left], array[right]]
+        } else if(currentSum < targetSum) {
+            left++
+        } else if(currentSum > targetSum) {
+            right--
+        }
+    }
+}
+
+
+twoSumArray([3,2,9,22,90,34,22,88,2], 5)
+
+
+// 
