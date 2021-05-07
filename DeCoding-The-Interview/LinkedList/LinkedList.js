01dd7fbbf094d429e7516f4130a3f27d64acb7f1
chaitanyamean <chaitanyamean@gmail.com>
2021-05-07 13:37:46 +0530

    LL insert method

--- DeCoding-The-Interview/LinkedList/LinkedList.js
+++ DeCoding-The-Interview/LinkedList/LinkedList.js
@@ -28,6 +28,39 @@ class LinkedList {
         this.head = newNode
         this.length++ 
     }
+
+    insert(idx, value) {
+        /* Adding a value to the start of LL */
+        if(idx === 0) {
+            this.prepend(value)
+            this.length++;
+        } 
+        /* Adding a value to the end of LL */
+        else if(idx >= this.length) {
+            this.append(value);
+            this.length++;            
+        } 
+        
+        else {
+            let newNode = new Node(value);
+            let leader = this.traverseNode(idx-1);
+            let holdingPointer = leader.next;
+            newNode.next = holdingPointer;
+            leader.next = newNode;
+            this.length++
+
+        }
+    }
+
+    traverseNode(idx) {
+        let count = 0
+        let currentNode = this.head
+        while (count !== idx) {
+            currentNode = currentNode.next;
+            count++
+        }
+        return currentNode
+    }
 }
 
 let ll = new LinkedList(10)
